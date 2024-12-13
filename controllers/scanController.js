ce6af4f4813916f44ef04ba973e3a542e3454593
blackbasilisk <dave@coates.co.za>
2024-12-13 15:51:05 +0200

    fixed error with saving new isPrint vars to DB.

--- controllers/scanController.js
+++ controllers/scanController.js
@@ -2,7 +2,7 @@ const { poolPromise, sql } = require('../db');
 var logger = require('../logger'); // Import the custom logger
 
 async function saveScanResult(scanInfo) {    
-  const { ScannedCode, isHRPrint, isBODPrint, isSCPrint, isEidosPrint } = scanInfo;
+  const { ScannedCode, isHRPrint, isBODPrint, isSCPrint, isEidosPrint, isLaserPrint, isDSPrint, isColorJetPrint } = scanInfo;
 
   if (!ScannedCode) {
     throw new Error('QR code is required');
@@ -19,7 +19,10 @@ async function saveScanResult(scanInfo) {
       .input('isBODPrint', sql.Bit, isBODPrint ? 1 : 0)
       .input('isSCPrint', sql.Bit, isSCPrint ? 1 : 0)
       .input('isEidosPrint', sql.Bit, isEidosPrint ? 1 : 0)
-      .query('INSERT INTO ScanResults (ScannedCode, IsHRPrint, IsBODPrint, IsSCPrint, IsEidosPrint) VALUES (@ScannedCode, @isHRPrint, @isBODPrint, @isSCPrint, @isEidosPrint)');
+      .input('isLaserPrint', sql.Bit, isLaserPrint ? 1 : 0)
+      .input('isDSPrint', sql.Bit, isDSPrint ? 1 : 0)
+      .input('isColorJetPrint', sql.Bit, isColorJetPrint ? 1 : 0)      
+      .query('INSERT INTO ScanResults (ScannedCode, IsHRPrint, IsBODPrint, IsSCPrint, IsEidosPrint, isLaserPrint, isDSPrint, isColorJetPrint) VALUES (@ScannedCode, @isHRPrint, @isBODPrint, @isSCPrint, @isEidosPrint, @isLaserPrint, @isDSPrint, @isColorJetPrint)');
 
     logger.info('QR code and checkbox selection saved successfully');   
     }    
